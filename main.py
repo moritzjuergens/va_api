617b7a938411de3d4013f5682801a77882fe5fc0
moritzjuergens <moritzjuergens@outlook.de>
2022-07-09 13:26:50 +0200

    t

--- main.py
+++ main.py
@@ -138,5 +138,6 @@ def post_questions():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7777))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get("PORT", 7777))
+    # app.run(host='0.0.0.0', port=port)
