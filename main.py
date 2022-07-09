32736ec067110b88212beb7bc20eca14b4fe5dde
moritzjuergens <moritzjuergens@outlook.de>
2022-07-09 14:45:46 +0200

    t

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def check_answer():
 
     try:
         game = client.postgrest.from_table("games").select(
-            "*").query("WHERE game_id=" + payload.game_id).execute()
+            "*").query("WHERE game_id='" + payload.game_id+"'").execute()
     except Exception as err:
         print(err)
         return Response(err, status=500)
